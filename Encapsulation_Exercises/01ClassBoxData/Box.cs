61e0d1045bfc857c0a86b8cb1bfda164f7202df9
Emil Stoykov <94859510+advanced-garbage@users.noreply.github.com>
2022-02-25 15:41:37 +0200

    Add files via upload

---
+++ Encapsulation_Exercises/01ClassBoxData/Box.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public double Length
+        {
+            get { return length; }
+            set
+            {
+                try
+                {
+                    if (value > 0)
+                        length = value;
+                    else
+                        throw new ArgumentException();
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("Length cannot be zero or negative.");
+                    Environment.Exit(0);
+                }
+            }
+        }
+        public double Width
+        {
+            get { return width; }
+            set
+            {
+                try
+                {
+                    if (value > 0)
+                        width = value;
+                    else
+                        throw new ArgumentException();
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("Width cannot be zero or negative.");
+                    Environment.Exit(0);
+                }
+            }
+        }
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                try
+                {
+                    if (value > 0)
+                        height = value;
+                    else
+                        throw new ArgumentException();
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("Height cannot be zero or negative.");
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        public Box(double l, double w, double h)
+        {
+            Length = l;
+            Width = w;
+            Height = h;
+        }
+
+        public double SurfaceArea() => (2 * Length * Width) + (2 * Height * Width) + (2 * Length * Height);
+
+        public double Volume() => Length * Width * Height;
+
+        public double LateralSurfaceArea() => (2 * Length * Height) + (2 * Width * Height);
+
+        public override string ToString()
+        {
+            return $"Surface Area - {SurfaceArea():f2}\nLateral Surface Area - {LateralSurfaceArea():f2}\nVolume - {Volume():f2}";
+        }
+
+    }
+}
---
+++ Encapsulation_Exercises/02AnimalFarm/Models/Chicken.cs
@@ -0,0 +1,94 @@
+﻿namespace AnimalFarm.Models
+{
+    public class Chicken
+    {
+        public const int MinAge = 0;
+        public const int MaxAge = 15;
+
+        private string name;
+        private int age;
+
+        internal Chicken(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            internal set
+            {
+                try
+                {
+                    if (string.IsNullOrWhiteSpace(value) || string.IsNullOrEmpty(value))
+                        throw new System.ArgumentException();
+                    this.name = value;
+                }
+                catch (System.ArgumentException)
+                {
+                    System.Console.WriteLine("Name cannot be empty.");
+                    System.Environment.Exit(0);
+                }
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            protected set
+            {
+                try
+                {
+                    if (value >= 0 && value <= 15)
+                        this.age = value;
+                    else
+                        throw new System.ArgumentException();
+                }
+                catch (System.ArgumentException)
+                {
+                    System.Console.WriteLine("Age should be between 0 and 15.");
+                    System.Environment.Exit(0);
+                }
+            }
+        }
+
+        public double ProductPerDay
+        {
+			get
+			{				
+				return this.CalculateProductPerDay();
+			}
+        }
+
+        private double CalculateProductPerDay()
+        {
+            switch (this.Age)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    return 1.5;
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                    return 2;
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                    return 1;
+                default:
+                    return 0.75;
+            }
+        }
+    }
+}
