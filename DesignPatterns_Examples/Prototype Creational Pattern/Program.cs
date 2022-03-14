d9cfe66fdc2107b08e0f2675bbee41a5fc032583
Emil Stoykov <94859510+advanced-garbage@users.noreply.github.com>
2022-03-14 15:07:07 +0200

    Add files via upload

---
+++ DesignPatterns_Examples/Prototype Creational Pattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using PrototypeDesignPattern.PrototypeClasses;
+
+namespace PrototypeDesignPattern
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ConcretePrototype1 p1 = new ConcretePrototype1("1");
+            ConcretePrototype1 c1 = (ConcretePrototype1)p1.Clone();
+            Console.WriteLine($"Cloned: {c1.Id}");
+
+            ConcretePrototype2 p2 = new ConcretePrototype2("2");   
+            ConcretePrototype2 c2 = (ConcretePrototype2)p2.Clone();
+            Console.WriteLine($"Cloned: {c2.Id}");
+        }
+    }
+}
---
+++ DesignPatterns_Examples/Prototype Creational Pattern/PrototypeClasses/ConcretePrototype2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrototypeDesignPattern.PrototypeClasses
+{
+    public class ConcretePrototype2 : Prototype
+    {
+        public ConcretePrototype2(string id) : base(id) { }
+        public override Prototype Clone()
+        {
+            return (Prototype)this.MemberwiseClone();
+        }
+    }
+}
